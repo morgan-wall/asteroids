ae2feaa8571afa13f46da3f09942d6d430319773
Morgan Wall <morganwall92@gmail.com>
2020-08-30 14:51:51 -0700

    Added rudimentary game flow controls

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using Unity.Entities;
+using Unity.Assertions;
+
+public class GameManager : MonoBehaviour
+{
+    private static readonly System.Type[] s_systemTypes = new System.Type[]
+    {
+        typeof(CollectionSystem),
+        typeof(CollisionSystem),
+        typeof(ConstantForceSystem),
+        typeof(DamageSystem),
+        typeof(EnemySystem),
+        typeof(FreezePositionSystem),
+        typeof(GameStateSystem),
+        typeof(MovableSystem),
+        typeof(PlayerSystem),
+        typeof(RandomGenSystem),
+        typeof(SpawnSystem),
+        typeof(WeaponSystem),
+    };
+
+    private void Awake()
+    {
+        var gameStateSystem = World.DefaultGameObjectInjectionWorld.GetExistingSystem<GameStateSystem>();
+        Assert.IsTrue(gameStateSystem != null, "Unable to retrieve GameStateSystem");
+        gameStateSystem.OnGameOver += OnGameOver;
+    }
+
+    private void Start()
+    {
+        EnableSystems(false);
+    }
+
+    private void EnableSystems(bool enabled)
+    {
+        foreach (System.Type type in s_systemTypes)
+        {
+            var system = World.DefaultGameObjectInjectionWorld.GetExistingSystem(type);
+            if (system != null)
+            {
+                system.Enabled = enabled;
+            }
+        }
+    }
+
+    private void OnGameOver()
+    {
+        EnableSystems(false);
+        Debug.Log("Game over!");
+    }
+}
--- Assets/Scripts/Systems/GameStateSystem.cs
+++ Assets/Scripts/Systems/GameStateSystem.cs
@@ -5,25 +5,37 @@ using Unity.Entities;
 using Unity.Jobs;
 using Unity.Mathematics;
 using Unity.Transforms;
+using System.Diagnostics.Tracing;
 
 [AlwaysUpdateSystem]
 [UpdateInGroup(typeof(PresentationSystemGroup))]
 public class GameStateSystem : SystemBase
 {
+    public delegate void GameOverDelegate();
+
+    public event GameOverDelegate OnGameOver;
+
     protected override void OnUpdate()
     {
-        EntityQuery collectableQuery = GetEntityQuery(ComponentType.ReadOnly<Collectable>());
-        if (collectableQuery.CalculateEntityCount() <= 0)
+        EntityQuery playerQuery = GetEntityQuery(ComponentType.ReadOnly<Player>());
+        if (playerQuery.CalculateEntityCount() > 0)
         {
-            Debug.Log("You won!");
+            return;
         }
-        else
+
+        var entityCommandBufferSystem = World.GetOrCreateSystem<BeginSimulationEntityCommandBufferSystem>();
+        var entityCommandBuffer = entityCommandBufferSystem.CreateCommandBuffer().AsParallelWriter();
+
+        Entities.ForEach((int entityInQueryIndex, Entity entity) =>
         {
-            EntityQuery playerQuery = GetEntityQuery(ComponentType.ReadOnly<Player>());
-            if (playerQuery.CalculateEntityCount() <= 0)
-            {
-                Debug.Log("You lose!");
-            }
+            entityCommandBuffer.AddComponent<Disabled>(entityInQueryIndex, entity);
+        }).ScheduleParallel();
+
+        entityCommandBufferSystem.AddJobHandleForProducer(Dependency);
+
+        if (OnGameOver != null)
+        {
+            OnGameOver();
         }
     }
 }
