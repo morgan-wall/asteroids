ae782ae89edf367f21adcdbcf090d463a7550069
Morgan Wall <morganwall92@gmail.com>
2020-08-30 16:28:07 -0700

    Added rudimentary camera following logic

---
+++ Assets/Scripts/Authoring/CameraFollowEntityAuthoring.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using Unity.Assertions;
+using Unity.Entities;
+
+[DisallowMultipleComponent]
+[RequiresEntityConversion]
+public class CameraFollowEntityAuthoring : MonoBehaviour, IConvertGameObjectToEntity
+{
+    public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
+    {
+        var camera = Camera.main;
+        if (camera == null)
+        {
+            return;
+        }
+
+        var cameraFollowEntity = camera.GetComponent<FollowEntity>();
+        if (cameraFollowEntity == null)
+        {
+            return;
+        }
+
+        cameraFollowEntity.EntityToFollow = entity;
+    }
+}
---
+++ Assets/Scripts/Utils/FollowEntity.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+public class FollowEntity : MonoBehaviour
+{
+    [SerializeField]
+    private bool m_followX = default;
+
+    [SerializeField]
+    private bool m_followY = default;
+
+    [SerializeField]
+    private bool m_followZ = default;
+
+    private EntityManager m_entityManager;
+
+    public Entity EntityToFollow { get; set; }
+
+    private void Awake()
+    {
+        m_entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
+    }
+
+    private void LateUpdate()
+    {
+        if (EntityToFollow == null)
+        {
+            return;
+        }
+
+        Vector3 newPosition = transform.position;
+        Translation entityTranslation = m_entityManager.GetComponentData<Translation>(EntityToFollow);
+        if (m_followX)
+        {
+            newPosition.x = entityTranslation.Value.x;
+        }
+        if (m_followY)
+        {
+            newPosition.y = entityTranslation.Value.y;
+        }
+        if (m_followZ)
+        {
+            newPosition.z = entityTranslation.Value.z;
+        }
+        transform.position = newPosition;
+    }
+}
